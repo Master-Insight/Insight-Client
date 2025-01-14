4d19ba9a5c9d43b622b69272964c2dbc6c069a16
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-01-14 01:11:51 -0300

    fix (Iconify): Add MDI library

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["*"], // (Default) Loads entire Material Design Icon set
+      }
+    }),
+  ]
 });
