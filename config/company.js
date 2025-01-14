b336326dccb0f0cf75c7dd0fd492e19f9209192c
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-01-14 00:59:07 -0300

    feat (config): company info

---
+++ config/company.js
@@ -0,0 +1,51 @@
+// los iconos son de https://icon-sets.iconify.design/
+
+const company = {
+  // Información de la empresa
+  info: {
+    name: "Insight",
+    description: "App Proyectos.",
+    address: "dirección",
+    whatsapp: "+54-93541-613185",
+  },
+  // Información para usar en las redes sociales (no whatsapp, si e-mail)
+  social: [
+    {
+      name: "Email",
+      href: "mailto:base.for.insight@gmail.com",
+      icon: "mdi:alternate-email", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+    {
+      name: "Facebook",
+      href: "https://www.facebook.com/",
+      icon: "mdi:facebook", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+    {
+      name: "X (Twitter)",
+      href: "https://twitter.com/",
+      icon: "mdi:twitter", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/",
+      icon: "mdi:instagram", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+    {
+      name: "YouTube",
+      href: "https://www.youtube.com/",
+      icon: "mdi:youtube", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/company/insight-desarrolladores/",
+      icon: "mdi:linkedin", // Reemplázalo por el nombre del ícono según tu sistema.
+    },
+  ],
+  images: {
+    logo: [
+      "/logos/Insight_hor_blanco.png"
+    ],
+  }
+}
+
+export default company
