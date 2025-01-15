22188ae70ed957cfdce2862674a9b385ec12e080
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-01-15 15:52:50 -0300

    ➕ feat (content): serviceCollection

---
+++ src/content/config.js
@@ -0,0 +1,25 @@
+import { defineCollection, z } from 'astro:content';
+
+// Esta colección luego se debe pedir a la BBDD
+const servicesCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    description: z.string(),
+    price: z.number(),
+    maintenance: z.number(),
+    image: z.string(),
+    characteristics: z.string(),
+    tags: z.array(z.string()),
+    serviceType: z.string(),
+    complexityLevel: z.enum(['low', 'medium', 'high']),
+    created: z.date(),
+    updated: z.date(),
+    active: z.boolean(),
+  }),
+});
+
+export const collections = {
+  services: servicesCollection,
+};
