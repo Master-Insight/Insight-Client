2f1af6ba076cd1752de08505fb6f79a204480cc1
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-01-28 16:33:43 -0300

    ➕ add (Page): escribenos

--- config/layout.js
+++ config/layout.js
@@ -5,7 +5,7 @@ const config = {
       { name: 'Servicios', path: '/services' },
       { name: 'Portfolio', path: '/samples' },
       { name: 'Conocenos', path: '/conocenos' },
-      // { name: 'Login', path: '/login' },
+      { name: 'Escribenos', path: '/escribenos' },
     ],
     public_It: [
       { name: 'Home', path: '/introit' },
