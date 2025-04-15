e689c0381bb30a4828e23e2382274c87cf4313ae
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-15 16:28:29 -0300

    📨 update (api): contact email funcional

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,37 +1,43 @@
 import type { APIRoute } from 'astro';
-import { EMAIL_USER_APP_1, EMAIL_PASS_APP_1,  EMAIL_USER_APP_2, EMAIL_PASS_APP_2 } from "astro:env/server"
+import { EMAIL_USER_APP_1, EMAIL_PASS_APP_1, EMAIL_USER_APP_2, EMAIL_PASS_APP_2 } from "astro:env/server"
 import nodemailer from 'nodemailer';
 
 const email = EMAIL_USER_APP_2;
 const password = EMAIL_PASS_APP_2;
 
+// 
+
 // Api: POST : Enviar email
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
-  const { name, emailto, message } = body;
+  const { emailfrom, name, emailto, message } = body;
 
-  // const transporter = nodemailer.createTransport({
-  //   service: 'gmail', //host: smt.gmail.com
-  //   port: 587,        // 465 + secure: true,
-  //   auth: {
-  //     user: email,
-  //     pass: password,
-  //   },
-  // });
   const transporter = nodemailer.createTransport({
-    service: 'c2790793.ferozo.com', //host: smt.gmail.com
-    port: 465,        // 465 + secure: true,
-    secure: true,
+    // Gmail no te deja enviar desde otro email que no sea EMAIL_USER_APP_1
+    service: 'gmail', //host: smt.gmail.com
+    port: 587,        // 465 + secure: true,
     auth: {
-      user: email,
-      pass: password,
+      user: EMAIL_USER_APP_1,
+      pass: EMAIL_PASS_APP_1,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
+  // const transporter = nodemailer.createTransport({
+  //   service: 'c2790793.ferozo.com', //host: smt.gmail.com
+  //   port: 465,        // 465 + secure: true,
+  //   secure: true,
+  //   auth: {
+  //     user: EMAIL_USER_APP_2,
+  //     pass: EMAIL_PASS_APP_2,
+  //   },
+  // });
 
   try {
     const mailOptions = {
-      from: '"InsightDev" <contacto@insightdevs.com.ar>',
-      to: emailto,
+      from: `"InsightDev" <EMAIL_USER_APP_1>`,
+      to: "gustavo.sirtori@gmail.com",
       subject: `Nuevo mensaje de ${name}`,
       text: message,
     }
@@ -43,6 +49,8 @@ export const POST: APIRoute = async ({ request }) => {
       { status: 200, headers: { 'Content-Type': 'application/json' } },
     );
   } catch (error) {
+    console.log(error);
+
     return new Response(
       JSON.stringify({ success: false, error }),
       { status: 500, headers: { 'Content-Type': 'application/json' } },
