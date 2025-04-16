b99f19d1aff7bcc1db6e609407326a5d9ddda30b
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-16 00:01:56 -0300

    📨 update (api): contact email template base

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,37 +2,44 @@ import type { APIRoute } from 'astro';
 import { EMAIL_USER_APP_1, EMAIL_PASS_APP_1, EMAIL_USER_APP_2, EMAIL_PASS_APP_2 } from "astro:env/server"
 import nodemailer from 'nodemailer';
 
-const email = EMAIL_USER_APP_2;
-const password = EMAIL_PASS_APP_2;
-
-// 
-
 // Api: POST : Enviar email
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
-  const { emailfrom, name, emailto, message } = body;
+  const { emailfrom, name, emailto, message, service = "donweb" } = body;
 
-  const transporter = nodemailer.createTransport({
-    // Gmail no te deja enviar desde otro email que no sea EMAIL_USER_APP_1
-    service: 'gmail', //host: smt.gmail.com
-    port: 587,        // 465 + secure: true,
-    auth: {
-      user: EMAIL_USER_APP_1,
-      pass: EMAIL_PASS_APP_1,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-  // const transporter = nodemailer.createTransport({
-  //   service: 'c2790793.ferozo.com', //host: smt.gmail.com
-  //   port: 465,        // 465 + secure: true,
-  //   secure: true,
-  //   auth: {
-  //     user: EMAIL_USER_APP_2,
-  //     pass: EMAIL_PASS_APP_2,
-  //   },
-  // });
+  function createTransport ( myservice ) {
+    if (myservice == "donweb") {
+      return  nodemailer.createTransport({
+        host: 'mail.insightdevs.com.ar', //host: smt.gmail.com
+        port: 465,        // 465 + secure: true,
+        secure: true,
+        auth: {
+          user: EMAIL_USER_APP_2,
+          pass: EMAIL_PASS_APP_2,
+        },
+        tls: {
+              rejectUnauthorized: false,
+            },
+      });
+    }
+    if (myservice == "gmail") {
+      return  nodemailer.createTransport({
+          // Gmail no te deja enviar desde otro email que no sea EMAIL_USER_APP_1
+          service: 'gmail', //host: smt.gmail.com
+          port: 587,        // 465 + secure: true,
+          auth: {
+            user: EMAIL_USER_APP_1,
+            pass: EMAIL_PASS_APP_1,
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        });
+    }
+  }
+
+  const email = service == "donweb" ? EMAIL_USER_APP_2 : EMAIL_USER_APP_1
+  const transporter = createTransport( service )
 
   // Precaucion adicional contra codigo malicioso
   const sanitize = (str: string) => str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -51,7 +58,7 @@ export const POST: APIRoute = async ({ request }) => {
 
   try {
     const mailOptions = {
-      from: `"InsightDev" <EMAIL_USER_APP_1>`,
+      from: `"InsightDev" <${email}>`,
       to: "gustavo.sirtori@gmail.com",
       subject: `📩 Nueva consulta de ${name}`,
       replyTo: emailto,
